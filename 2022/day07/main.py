25e4ea3738afe01ae6a1be913f8fe05652cb2699
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2022-12-07 15:53:50 +0100

    Solution to puzzle 7

--- 2022/day07/main.py
+++ 2022/day07/main.py
@@ -1,5 +1,10 @@
+import os
+
 with open("input.txt", "r") as f:
-    inputs = [[j.split(" ") for j in i.replace("$", "").strip().split("\n")] for i in f.read().split("\n$")]
+    inputs = [
+        [j.split(" ") for j in i.replace("$", "").strip().split("\n")]
+        for i in f.read().split("\n$")
+    ]
 
 results = {}
 paths = []
@@ -7,20 +12,23 @@ for i in inputs:
     if i[0][0] == "cd" and i[0][1] == "..":
         paths.pop()
     elif i[0][0] == "cd":
-        currentPath = "{}{}/".format(paths[-1], i[0][1]) if len(paths) > 0 else i[0][1]
-        paths += [currentPath]
-        results[currentPath] = 0
+        paths.append(
+            os.path.join(paths[-1], i[0][1]) if len(paths) > 0 else i[0][1]
+        )
+        results[paths[-1]] = 0
     else:
-        total = sum([int(y[0]) for y in filter(lambda x: x[0] != "ls" and x[0] != "dir", i)])
+        total = sum([int(y[0]) for y in [x for x in i[1:] if x[0] != "dir"]])
         for p in paths:
             results[p] += total
 
 # Part1
-total = sum(filter(lambda r: r <= 100000, results.values()))
-print("Total <= 100000: {}".format(total))
+max = 100000
+total = sum([v for v in results.values() if v <= max])
+print("Total #1: {}".format(total))
 
 # Part2
-available = 70000000-results["/"]
-needed = 30000000-available
-toDel = min([*filter(lambda r: r[1] > needed, zip(results.keys(), results.values()))], key=lambda r: r[1])
-print("To delete: {} {}".format(toDel[0], toDel[1]))
+available = 70000000
+required = 30000000
+needed = required-(available-results["/"])
+total = min([v for v in results.values() if v > needed])
+print("Total #2: {}".format(total))
