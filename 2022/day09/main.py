b54876ffb695c31bb04973dc24df92923c743b59
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2022-12-09 11:05:06 +0100

    Solution to puzzle 9

--- 2022/day09/main.py
+++ 2022/day09/main.py
@@ -1,56 +1,40 @@
-import math
 import numpy as np
 
 with open("input.txt", "r") as f:
     inputs = [(i.split(" ")[0], int(i.split(" ")[1]))
               for i in f.read().splitlines()]
 
+MOVES = {
+  "R": np.array([1, 0]),
+  "L": np.array([-1, 0]),
+  "U": np.array([0, 1]),
+  "D": np.array([0, -1])
+}
+
 def solve(length):
     moves = []
-    for i in range(0, length):
-        moves.append([(0, 0)])
+    for _ in range(0, length):
+        moves.append([np.array([0, 0])])
 
     def updateTail():
-        for i in range(1, len(moves)):
+        for i, move in enumerate(moves[1:], start=1):
             offset = (0, 0)
-            if math.dist(moves[i-1][-1], moves[i][-1]) >= 2:
-                offset = (
-                    np.sign(moves[i-1][-1][0] - moves[i][-1][0]),
-                    np.sign(moves[i-1][-1][1] - moves[i][-1][1])
-                )
+            if np.linalg.norm(moves[i-1][-1] - move[-1]) >= 2:
+                offset = np.array([
+                    np.sign(moves[i-1][-1][0] - move[-1][0]),
+                    np.sign(moves[i-1][-1][1] - move[-1][1])
+                ])
             if offset[0] != 0 or offset[1] != 0:
-                moves[i].append((
-                    moves[i][-1][0] + offset[0],
-                    moves[i][-1][1] + offset[1]
-                ))
+                move.append(move[-1] + offset)
 
     for input in inputs:
-        for i in range(0, input[1]):
-            if input[0] == "R":
-                moves[0].append((
-                    moves[0][-1][0] + 1,
-                    moves[0][-1][1]
-                ))
-            elif input[0] == "L":
-                moves[0].append((
-                    moves[0][-1][0] - 1,
-                    moves[0][-1][1]
-                ))
-            elif input[0] == "U":
-                moves[0].append((
-                    moves[0][-1][0],
-                    moves[0][-1][1] + 1
-                ))
-            else:
-                moves[0].append((
-                    moves[0][-1][0],
-                    moves[0][-1][1] - 1
-                ))
+        for _ in range(0, input[1]):
+            moves[0].append(moves[0][-1] + MOVES[input[0]])
             updateTail()
 
-    return len(set(["{},{}".format(t[0], t[1]) for t in moves[-1]]))
+    return len(set([tuple(t) for t in moves[-1]]))
 
 # Part 1
-print("Visited once: {}".format(solve(2)))
+print("Visited once #1: {}".format(solve(2)))
 # Part 2
-print("Visited once: {}".format(solve(10)))
+print("Visited once #2: {}".format(solve(10)))
