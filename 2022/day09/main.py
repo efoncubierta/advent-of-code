c36662c9de74798033e1e5c60ccb32ebfa417f82
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2022-12-09 10:32:30 +0100

    Solution to puzzle 9

--- 2022/day09/main.py
+++ 2022/day09/main.py
@@ -2,58 +2,55 @@ import math
 import numpy as np
 
 with open("input.txt", "r") as f:
-    moves = [(i.split(" ")[0], int(i.split(" ")[1]))
-             for i in f.read().splitlines()]
+    inputs = [(i.split(" ")[0], int(i.split(" ")[1]))
+              for i in f.read().splitlines()]
+
+def solve(length):
+    moves = []
+    for i in range(0, length):
+        moves.append([(0, 0)])
+
+    def updateTail():
+        for i in range(1, len(moves)):
+            offset = (0, 0)
+            if math.dist(moves[i-1][-1], moves[i][-1]) >= 2:
+                offset = (
+                    np.sign(moves[i-1][-1][0] - moves[i][-1][0]),
+                    np.sign(moves[i-1][-1][1] - moves[i][-1][1])
+                )
+            if offset[0] != 0 or offset[1] != 0:
+                moves[i].append((
+                    moves[i][-1][0] + offset[0],
+                    moves[i][-1][1] + offset[1]
+                ))
+
+    for input in inputs:
+        for i in range(0, input[1]):
+            if input[0] == "R":
+                moves[0].append((
+                    moves[0][-1][0] + 1,
+                    moves[0][-1][1]
+                ))
+            elif input[0] == "L":
+                moves[0].append((
+                    moves[0][-1][0] - 1,
+                    moves[0][-1][1]
+                ))
+            elif input[0] == "U":
+                moves[0].append((
+                    moves[0][-1][0],
+                    moves[0][-1][1] + 1
+                ))
+            else:
+                moves[0].append((
+                    moves[0][-1][0],
+                    moves[0][-1][1] - 1
+                ))
+            updateTail()
+
+    return len(set(["{},{}".format(t[0], t[1]) for t in moves[-1]]))
 
 # Part 1
-#length = 2
-
+print("Visited once: {}".format(solve(2)))
 # Part 2
-length = 10
-
-positions = []
-for i in range(0, length):
-    positions.append([(0, 0)])
-
-
-def updateTail():
-    for i in range(1, len(positions)):
-        offset = (0, 0)
-        if math.dist(positions[i-1][-1], positions[i][-1]) >= 2:
-            offset = (
-                np.sign(positions[i-1][-1][0] - positions[i][-1][0]),
-                np.sign(positions[i-1][-1][1] - positions[i][-1][1])
-            )
-        if offset[0] != 0 or offset[1] != 0:
-            positions[i].append((
-                positions[i][-1][0] + offset[0],
-                positions[i][-1][1] + offset[1]
-            ))
-
-
-for move in moves:
-    for i in range(0, move[1]):
-        if move[0] == "R":
-            positions[0].append((
-                positions[0][-1][0] + 1,
-                positions[0][-1][1]
-            ))
-        elif move[0] == "L":
-            positions[0].append((
-                positions[0][-1][0] - 1,
-                positions[0][-1][1]
-            ))
-        elif move[0] == "U":
-            positions[0].append((
-                positions[0][-1][0],
-                positions[0][-1][1] + 1
-            ))
-        else:
-            positions[0].append((
-                positions[0][-1][0],
-                positions[0][-1][1] - 1
-            ))
-        updateTail()
-
-once = len(set(["{},{}".format(t[0], t[1]) for t in positions[-1]]))
-print("Visited once: {}".format(once))
+print("Visited once: {}".format(solve(10)))
