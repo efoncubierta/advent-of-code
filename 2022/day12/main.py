b1440a77c5afc556c4ef8450a5f06a4a634072a3
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2022-12-12 17:29:29 +0100

    Solution to puzzle 12

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -8,42 +8,37 @@ start = [
 inputs[start[0]][start[1]] = 'a'
 
 
-def isAvailable(p, n, D):
-    return (
-        n[0] >= 0 and
-        n[0] < len(inputs) and
-        n[1] >= 0 and
-        n[1] < len(inputs[0])
-    ) and (
-        'a' <= inputs[n[0]][n[1]] <= chr(ord(inputs[p[0]][p[1]]) + 1) or
-        (
-            inputs[p[0]][p[1]] == 'z' and inputs[n[0]][n[1]] == 'E'
-        )
-    ) and D[n[0]][n[1]] == 0
-
-
-def minDistance(coord):
+def minDistance(init):
     D = [[0 for _ in range(len(inputs[0]))] for _ in range(len(inputs))]
 
-    queue = [(coord[0], coord[1], 0)]
-    D[coord[0]][coord[1]] = 1
+    queue = [(init[0], init[1], 0)]
+    D[init[0]][init[1]] = 1
     while len(queue) != 0:
         p = queue.pop(0)
 
         if inputs[p[0]][p[1]] == 'E':
             return p[2]
 
-        nexts = [
+        for n in [
             (n[0], n[1], p[2] + 1)
             for n in [
                 (p[0], p[1] + 1),
                 (p[0], p[1] - 1),
                 (p[0] + 1, p[1]),
                 (p[0] + -1, p[1]),
-            ] if isAvailable((p[0], p[1]), n, D)
-        ]
-
-        for n in nexts:
+            ]
+            if (
+                n[0] >= 0 and
+                n[0] < len(inputs) and
+                n[1] >= 0 and
+                n[1] < len(inputs[0])
+            ) and (
+                'a' <= inputs[n[0]][n[1]] <= chr(ord(inputs[p[0]][p[1]]) + 1) or
+                (
+                    inputs[p[0]][p[1]] == 'z' and inputs[n[0]][n[1]] == 'E'
+                )
+            ) and D[n[0]][n[1]] == 0
+        ]:
             queue.append(n)
             D[n[0]][n[1]] = 1
 
