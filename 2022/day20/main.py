83759dacdfa4ce05300e8d2a3efb410077cb829d
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2022-12-20 13:55:30 +0100

    Solution to puzzle 20

--- 2022/day20/main.py
+++ 2022/day20/main.py
@@ -1,12 +1,12 @@
 with open("input.txt", "r") as f:
     values = [
-        (i, int(v)) # same value can appear multiple times, add index
-        for i, v in enumerate(f.read().splitlines())
+        int(v)
+        for v in f.read().splitlines()
     ]
 
 def decrypt(key, n):
-    # decrypted values
-    d_values = [(i[0], key*i[1]) for i in values]
+    # decrypted values. same value can appear multiple times, add index
+    d_values = [(i, key*v) for i, v in enumerate(values)]
     # sorted values
     s_values = d_values.copy()
     for _ in range(n):
@@ -16,8 +16,7 @@ def decrypt(key, n):
             vi = (vi + v[1]) % len(s_values)
             # index correction
             # according to instructions, values at 0 index are moved to the tail
-            vi = len(s_values) if vi == 0 else vi
-            s_values.insert(vi, v)
+            s_values.insert(len(s_values) if vi == 0 else vi, v)
 
     zi = [i for i, v in enumerate(s_values) if v[1] == 0][0]
     total = 0
