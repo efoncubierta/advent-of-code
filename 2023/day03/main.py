da10a6c2213d0cefecc0bb92939a7ea52fb3dc2d
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2023-12-03 19:41:05 +0100

    Clean code

--- 2023/day03/main.py
+++ 2023/day03/main.py
@@ -4,24 +4,46 @@ from functools import reduce
 with open("input.txt", "r") as f:
     inputs = f.read().split("\n")
 
+# height and width
 h = len(inputs)
 w = len(inputs[0])
-    
-def scan(y, x_start, x_finish, f):
-    for x_i in range(x_start, x_finish + 1):
-        if f(y, x_i):
+
+# check if in a row, the check_f test passes
+def in_row(row, col_start, col_finish, check_f):
+    for col in range(col_start, col_finish + 1):
+        if check_f(row, col):
             return True
     return False
 
-is_number = lambda y, x: inputs[y][x] >= '0' and inputs[y][x] <= '9'
-is_symbol = lambda y, x: inputs[y][x] != '.' and not is_number(y, x)
-is_gear   = lambda y, x: inputs[y][x] == '*'
+# check if check_f passes in range
+def in_range(row, col_start, col_finish, check_f):
+    return (
+        row > 0 and 
+        in_row(row-1, max(0, col_start - 1), min(w-1, col_finish + 1), check_f)
+    ) or (
+        row < (h-1) and
+        in_row(row+1, max(0, col_start - 1), min(w-1, col_finish + 1), check_f)
+    ) or (
+        col_start > 0 and
+        check_f(row, col_start - 1)
+    ) or (
+        col_finish < (w-1) and
+        check_f(row, col_finish + 1)
+    )
 
+# check whether a position is a number, symbol or gear
+is_number = lambda row, col: inputs[row][col] >= '0' and inputs[row][col] <= '9'
+is_symbol = lambda row, col: inputs[row][col] != '.' and not is_number(row, col)
+is_gear   = lambda row, col: inputs[row][col] == '*'
+
+# total accumulated in calculations
 total = 0
+
+# get all numbers by row, column start and column finish
 numbers = [
-    (y, x_start, x_finish, int(input[x_start:x_finish+1]))
-    for y, input in enumerate(inputs)
-    for (x_start, x_finish) in [
+    (row, col_start, col_finish, int(input[col_start:col_finish+1]))
+    for row, input in enumerate(inputs)
+    for (col_start, col_finish) in [
         (m.start(), m.end() - 1)
         for m in re.finditer("\d+", input)
     ]
@@ -29,14 +51,9 @@ numbers = [
 
 # Part 1
 #
-for (y, x_start, x_finish, n) in numbers:
+for (row, col_start, col_finish, n) in numbers:
     # only numbers with symbols in range are considered
-    if (
-        (y > 0 and scan(y-1, max(0, x_start - 1), min(w-1, x_finish + 1), is_symbol)) or
-        (y < (h-1) and scan(y+1, max(0, x_start - 1), min(w-1, x_finish + 1), is_symbol)) or
-        (x_start > 0 and is_symbol(y, x_start - 1)) or
-        (x_finish < (w-1) and is_symbol(y, x_finish + 1))
-    ):
+    if in_range(row, col_start, col_finish, is_symbol):
         total += n
 
 print("Part 1: {}".format(total))
@@ -45,22 +62,22 @@ print("Part 1: {}".format(total))
 #
 total = 0
 gears = [
-    (y, x)
-    for y in range(h)
-    for x in range(w)
-    if is_gear(y, x)
+    (row, col)
+    for row in range(h)
+    for col in range(w)
+    if is_gear(row, col)
 ]
-for (y, x) in gears:
+for (row, col) in gears:
     # find all numbers in gear's range
     ns = [
-        n[3]
-        for n in numbers if (
-            (n[1]-1 <= x <= n[2]+1) and
-            (n[0]-1 <= y <= n[0]+1)
+        n
+        for (n_row, n_col_start, n_col_finish, n) in numbers if (
+            (n_col_start-1 <= col <= n_col_finish+1) and
+            (n_row-1 <= row <= n_row+1)
         )
     ]
     # only gears with two numbers in range are considered
     if len(ns) == 2:
-        total += reduce((lambda x, y: x * y), ns)
+        total += reduce((lambda n_acc, n: n_acc * n), ns)
 
 print("Part 2: {}".format(total))
