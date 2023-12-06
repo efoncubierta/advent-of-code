9cb4c32cef56af6af0f0c6014a0b206e759bd6df
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2023-12-06 14:06:03 +0100

    Clean code

--- 2023/day06/main.py
+++ 2023/day06/main.py
@@ -7,28 +7,18 @@ with open("input.txt", "r") as file:
         for line in file.readlines()
       ]
 
-solutions = lambda t, d: [
-    math.floor(((-t - math.sqrt(pow(t, 2) - 4*d))/-2)- 0.001),
-    math.ceil(((-t + math.sqrt(pow(t, 2) - 4*d))/-2) + 0.001)
+solve = lambda t, d: [
+    math.floor(((-t - math.sqrt(pow(t, 2) - 4*d))/-2) - 0.1),
+    math.ceil(((-t + math.sqrt(pow(t, 2) - 4*d))/-2) + 0.1)
 ]
 
 # Part1
-p1_inputs = [
-    [int(d) for d in input]
-    for input in inputs
-]
 total = 1
-for i in range(len(p1_inputs[0])):
-    s = solutions(p1_inputs[0][i], p1_inputs[1][i])
+for i in range(len(inputs[0])):
+    s = solve(int(inputs[0][i]), int(inputs[1][i]))
     total *= s[0] - s[1] + 1
-
 print("Part 1: {}".format(total))
 
 # Part 2
-p2_inputs = [
-    ''.join(input)
-    for input in inputs
-]
-
-s = solutions(int(p2_inputs[0]), int(p2_inputs[1]))
+s = solve(int(''.join(inputs[0])), int(''.join(inputs[1])))
 print("Part 2: {}".format(s[0] - s[1] + 1))
