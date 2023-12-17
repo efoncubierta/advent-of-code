d60d07af4150f144e3d8d09faf901482ceddcae0
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2023-12-17 12:31:21 +0100

    Clean code

--- 2023/day17/main.py
+++ 2023/day17/main.py
@@ -18,25 +18,32 @@ def calc(part2 = False):
 
     # priority queue
     pq = []
+
     # start state: (heat loss, x, y, direction, moves)
     heapq.heappush(pq, (grid[0][0] if part2 else 0, 0, 0, 0, 1))
 
-    # states visited (x, y, direction, consecutive_moves)
+    # states visited (x, y, direction, moves)
     states = {}
 
     while pq:
         hl, x, y, d, m = heapq.heappop(pq)
 
-        # mark the current state as visited
+        # return bottom-right corner
+        if x == w-1 and y == h-1:
+            return hl
+        
+        # ignore visited states
         if (x, y, d, m) in states:
             continue
 
+        # mark the current state as visited
         states[(x, y, d, m)] = hl
 
-        # move all directions but backwards
+        # move in all directions but backwards
         for i in range(3):
             new_d = (d + i - 1) % 4
             
+            # move
             nx, ny = x + dx[new_d], y + dy[new_d]
             new_m = m + 1 if new_d == d else 1
 
@@ -46,17 +53,11 @@ def calc(part2 = False):
                 (part2 and new_m <= 10 and (new_d == d or m >= 4))
             )
             
+            # add new state if within bounds and allowed
             if 0 <= nx < w and 0 <= ny < h and allowed:
                 new_hl = hl + grid[ny][nx]
                 new_s = (new_hl, nx, ny, new_d, new_m)
                 heapq.heappush(pq, new_s)
-
-    total = float('inf')
-    for s in states:
-        if s[0] == w-1 and s[1] == h-1:
-            total = min(total, states[s])
-
-    return total
 
 print("Part 1: {}".format(calc()))
 print("Part 2: {}".format(calc(True)))
