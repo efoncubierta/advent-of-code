93edcfd8cfefb7bcc12c4cc1156c9110280db55d
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2023-12-20 12:16:23 +0100

    Clean code

--- 2023/day20/main.py
+++ 2023/day20/main.py
@@ -20,40 +20,44 @@ with open("input.txt", "r") as file:
         for input in file.read().split("\n")
     ]
 
-modules = {
+# modules
+M = {
     i[0]: [i[1], i[2], False]
     for i in inputs
 }
 
-conjunctions = {
-    m1: {m2: False for m2 in modules if m1 in modules[m2][1]}
-    for m1 in modules
-    if modules[m1][0] == '&'
+# conjunctions
+C = {
+    m1: {m2: False for m2 in M if m1 in M[m2][1]}
+    for m1 in M
+    if M[m1][0] == '&'
 }
 
+# states for part2 counting
 PART2_S = {
     c: [0, 0]
-    for c in conjunctions['df']
+    for c in C['df']
 }
 
 def solve(part2 = False):
-    C = (0, 0) # signals counter (high, low)
+    S = (0, 0) # signals counter (high, low)
     N = 0      # button pushes
     LCM = []   # in part1, least-common divisor items
     while N == 0 or (not part2 and N < 1000) or (part2 and N < 100000000):
-        Q = deque([('broadcaster', modules['broadcaster'][1], False)])
-        C = (C[0], C[1]+1)
+        # queue (source, targets, signal)
+        Q = deque([('broadcaster', M['broadcaster'][1], False)])
+        S = (S[0], S[1]+1)
         N += 1
-
         while Q:
-            source, mods, sig = Q.popleft()
+            source, targets, sig = Q.popleft()
 
-            C = (
-                C[0] + (1*len(mods) if sig else 0),
-                C[1] + (1*len(mods) if not sig else 0)
+            # increment signal counter
+            S = (
+                S[0] + (1*len(targets) if sig else 0),
+                S[1] + (1*len(targets) if not sig else 0)
             )
 
-            for mod in mods:
+            for mod in targets:
                 # part 2
                 if part2:
                     if mod in PART2_S and not sig:
@@ -66,18 +70,18 @@ def solve(part2 = False):
                         return lcm(LCM)
                 
                 # ignore test modules
-                if mod not in modules:
+                if mod not in M:
                     continue
 
-                type, dest, state = modules[mod]
+                type, dest, state = M[mod]
                 if type == '%' and not sig:
-                    modules[mod][2] = not state
+                    M[mod][2] = not state
                     Q.append((mod, dest, not state))
                 elif type == '&':
-                    conjunctions[mod][source] = sig
-                    Q.append((mod, dest, not all(conjunctions[mod].values())))
+                    C[mod][source] = sig
+                    Q.append((mod, dest, not all(C[mod].values())))
 
-    return C[0] * C[1]
+    return S[0] * S[1]
                     
 print("Part 1: {}".format(solve()))
 print("Part 2: {}".format(solve(True)))
