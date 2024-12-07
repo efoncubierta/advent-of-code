a621c0bc9e29280e57b30fc2bebcd7be5bc4ad39
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2024-12-07 10:40:42 +0100

    clean code

--- 2024/day07/main.py
+++ 2024/day07/main.py
@@ -6,33 +6,28 @@ for i in input:
     p = i.split(": ")
     equations.append([int(n) for n in ([p[0]] + p[1].split(" "))])
 
-def solve(elems, total, part2 = False):
+def has_solution(elems, total, part2 = False):
     if elems[0] > total:
-        return None
+        return False
     
     if len(elems) == 1:
-        return elems[0] if elems[0] == total else None
+        return elems[0] == total
     
     e1 = elems.pop(0)
     e2 = elems.pop(0)
     
-    r = solve([e1 + e2] + elems, total, part2)
-    if r == total:
-        return r
-    
-    r = solve([e1 * e2] + elems, total, part2)
-    if r == total:
-        return r
-    
-    return solve([int("{}{}".format(e1,e2))] + elems, total, part2) if part2 else r
+    return has_solution([e1 + e2] + elems, total, part2) or \
+        has_solution([e1 * e2] + elems, total, part2) or \
+        (
+            part2 and
+            has_solution([int("{}{}".format(e1,e2))] + elems, total, part2)
+        )
         
-
-total1 = 0
-total2 = 0
+total1, total2 = 0, 0
 for eq in equations:
-    if solve(eq[1:], eq[0]):
+    if has_solution(eq[1:], eq[0]):
         total1 += eq[0]
-    if solve(eq[1:], eq[0], part2=True):
+    if has_solution(eq[1:], eq[0], part2=True):
         total2 += eq[0]
 
 print("Part 1: {}".format(total1))
