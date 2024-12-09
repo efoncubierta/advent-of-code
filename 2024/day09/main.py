1e2981416bc194d4d994f4deefe6455283e5bdb7
Ezequiel Foncubierta <ezequiel@foncubierta.com>
2024-12-09 17:12:05 +0100

    Clean code

--- 2024/day09/main.py
+++ 2024/day09/main.py
@@ -6,15 +6,18 @@ id = -1
 for i in range(0, len(input), 2):
     id += 1
     FILES += [id] * input[i]
-    FILES += [-1] * input[i+1] if i+1 < len(input) else []
+    FILES += [-1] * input[i+1] if i+1 < len(input) else [] # free space is -1
     SIZES[id] = input[i]
 
 def sort1(files):
     i, j = 0, len(files) - 1
     while i < j:
+        # find forward next free space
         if files[i] < 0:
+            # find backwards next file to move
             while files[j] < 0:
                 j -= 1
+            # move
             files[i], files[j] = files[j], -1
             j -= 1
         i += 1
@@ -41,7 +44,7 @@ def sort2(files):
             while (space_i + space_s) < len(files) and files[space_i + space_s] < 0:
                 space_s += 1
             
-            # move if file fits in free space
+            # move if file fits in free space, and jump to next file
             if space_i < file_i and space_s >= file_s:
                 for jj in range(file_s):
                     files[space_i+jj] = files[file_i+jj]
@@ -55,12 +58,7 @@ def sort2(files):
         file_i -= 1
 
 def checksum(files):
-    sum = 0
-    for i in range(len(files)):
-        if files[i] < 0:
-            continue
-        sum += (i * int(files[i]))
-    return sum
+    return sum([(i * int(files[i])) for i in range(len(files)) if files[i] >= 0])
 
 files = FILES.copy()
 sort1(files)
